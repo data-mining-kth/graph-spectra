672cc639208a5b39078c048a617ac5d80994c07b
Brando Chiminelli <brando.chiminelli99@gmail.com>
2022-11-30 10:02:03 +0100

    add fiedler vector and correct clustering

--- src/spectral_clustering.m
+++ src/spectral_clustering.m
@@ -1,43 +1,43 @@
-%% Implementation of the algorithm
-% Given the set of points S
-S = readmatrix('../data/example1.dat');
-n = size(S, 1);
-x_coord = S(:,1);
-y_coord = S(:,2);
-% Form the affinity matrix A
-% It is computing the distance between points
-% the smaller the distance between point, the higher exp()
-% set to zero the distance between a point and itself
-% because it would  be one otherwise
-% The higher the value of A(i,j) the higher the affinity
-A = zeros(n,n);
-sigma = 3;
-for i = 1:n
-    for j = 1:n
-        A(i,j) = exp(-norm(S(i,:)-S(j,:), 2)/(2*sigma^2));
-        if  i == j
-            A(i,j) = 0;
-        end
-    end
-end
-figure(1)
-imagesc(A)
-%% Define D diagonal matrix and build Laplacian
-D = zeros(n,n);
-for i = 1:n
-    for j = 1:n
-        if i == j
-        D(i,j) = sum(A(i,:));
-        end
-    end
-end
-% Construct matrix L, Laplacian matrix
-% L is symmetrically normalized Laplacian
-% it measures the extent to which a point
-% differs from its neighbors
-L = sqrt(inv(D))*A*sqrt(inv(D));
-%% Form the matrix X of max eigenvectors
-k = 10;
-eig_vect = eig(A);
-figure()
-imagesc(A)
+clear all;close all;clc;
+
+K=4; % Number of clusters
+
+E = readmatrix('../data/example1.dat');
+E = unique(E, "rows");
+
+col1 = E(:,1);
+col2 = E(:,2);
+max_ids = max(max(col1,col2));
+As = sparse(col1, col2, 1, max_ids, max_ids); % Computation of the adjacency matrix
+
+G = graph (As);
+
+A = full(As); % To not have a sparse matrix
+
+figure
+imagesc(A) % Display sparsity matrix
+
+D = diag(sum(A,2)); % Diagonal matrix algorithm
+L = D^(-1/2)*A*D^(-1/2);
+
+[Vl,Dl] = eig(L);
+% Fiedler Vector is the vector corresponding to the second smallest eigenvalue of the Laplacian matrix, L
+fiedlerVec = Vl(:,2); 
+sorted_fielderVec = sort(fiedlerVec);
+figure;
+plot(sorted_fielderVec)
+
+[VK,DK] = eigs(L,K,'largestreal');
+D = diag(DK);
+
+denom  =(sum( VK.^2,2)).^(1/2);
+Y = bsxfun(@rdivide,VK,denom);
+
+idx = kmeans(Y,K);
+
+figure;
+h = plot(G);
+highlight(h,find(idx==1),'NodeColor','g')
+highlight(h,find(idx==2),'NodeColor','r')
+highlight(h,find(idx==3),'NodeColor','b')
+highlight(h,find(idx==4),'NodeColor','c')
